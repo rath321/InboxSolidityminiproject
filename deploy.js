767f873bc6df6a5480cf89f5a4ba5ace195ab720
rath321 <60335350+rath321@users.noreply.github.com>
2021-06-09 13:46:06 +0530

    Add files via upload

---
+++ deploy.js
@@ -0,0 +1,19 @@
+const HDWalletProvider= require('truffle-hdwallet-provider');
+const Web3=require('web3');
+const {interface, bytecode}=require('./compile');
+const provider=new HDWalletProvider(
+
+'helmet lamp valve enable blush physical thing sail drastic when trip carpet','https://rinkeby.infura.io/v3/9a00e60a1b984d588ad6b06918289233'
+);
+const web3=new Web3(provider);
+
+
+const deploy=async()=>{
+const accounts= await web3.eth.getAccounts();
+console.log('Attempting to deploy from account',accounts[0]);
+const result=await new web3.eth.Contract(JSON.parse(interface)).deploy({
+    data:bytecode,arguments:['Hi there']
+}).send({gas:'1000000',from:accounts[0]});
+console.log('Contract deployed to',result.options.address);
+};
+deploy();
